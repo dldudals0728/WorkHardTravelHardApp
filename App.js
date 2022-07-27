a69ceb72d91155dcbb61c811aad097c5e539378e
devid <dldudals0728@naver.com>
2022-07-27 18:23:16 +0900

    #3.2 TextInput

--- App.js
+++ App.js
@@ -1,35 +1,50 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
-  TouchableHighlight,
-  TouchableWithoutFeedback,
+  TextInput,
 } from "react-native";
 import { theme } from "./colors";
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
   return (
     <View style={styles.container}>
+      <StatusBar style="light" />
       <View style={styles.header}>
-        {/* TouchableOpacity: 누르는 event를 listen할 준비가 된 view. Opacity(투명도)는, 누를 때 투명해지는 애니메이션이 있다는 뜻! */}
-        {/* Pressable: new! more props */}
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? "white" : theme.grey }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
-        {/* TouchableHighlight: TouchableOpacity와 다르게, 눌렸을 때 background에 효과를 준다. */}
-        {/* TouchableWithoutFeedback: 어떤 애니메이션도 없음. */}
-        <TouchableOpacity
-          activeOpacity={0.5}
-          onPress={() => {
-            console.log("pressed!");
-          }}
-        >
-          <Text style={styles.btnText}>Travel</Text>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
         </TouchableOpacity>
       </View>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput
+          onChangeText={onChangeText}
+          value={text}
+          placeholder={working ? "Add a To Do" : "Where do you want to go?"}
+          style={styles.input}
+        />
+      </View>
     </View>
   );
 }
@@ -50,5 +65,13 @@ const styles = StyleSheet.create({
     fontSize: 38,
     fontWeight: "600",
     color: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
