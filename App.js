2d579fd07216dd6717de8399b7ac1ec855005a64
devid <dldudals0728@naver.com>
2022-07-27 17:53:37 +0900

    #3.1 Touchables TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable: new

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { theme } from "./colors";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        {/* TouchableOpacity: 누르는 event를 listen할 준비가 된 view. Opacity(투명도)는, 누를 때 투명해지는 애니메이션이 있다는 뜻! */}
+        {/* Pressable: new! more props */}
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        {/* TouchableHighlight: TouchableOpacity와 다르게, 눌렸을 때 background에 효과를 준다. */}
+        {/* TouchableWithoutFeedback: 어떤 애니메이션도 없음. */}
+        <TouchableOpacity
+          activeOpacity={0.5}
+          onPress={() => {
+            console.log("pressed!");
+          }}
+        >
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +37,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    // paddingHorizontal: css문법이 아니고, react native에서 제공하는 기능!
+    paddingHorizontal: 20,
+  },
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+  btnText: {
+    fontSize: 38,
+    fontWeight: "600",
+    color: "white",
   },
 });
